a2671e70bb61f5354b2d1456ab320374b672fadb
ceanil <58513321+ceanil@users.noreply.github.com>
2019-12-12 22:22:34 +0530

    Update Week3_Assignment.py

--- Week3_Assignment.py
+++ Week3_Assignment.py
@@ -1,2 +1,4 @@
 print('Capstone Week 3 part 1')
 https://eu-gb.dataplatform.cloud.ibm.com/analytics/notebooks/v2/2989a0ba-ad1c-4e98-b8ee-f939f24ac36e/view?access_token=d0962c70ff6ec8fb1d1d85a5701693ff496eede43e6b9bc8e6922bef7c741c86
+https://eu-gb.dataplatform.cloud.ibm.com/analytics/notebooks/v2/ab46be37-53c4-4841-9a89-07b59cfd99e8/view?access_token=aa1411bdb2393b4d0f6f8bd35e904ef504e0eeae1f7bbd3c1939b7839c08c7f6
+https://eu-gb.dataplatform.cloud.ibm.com/analytics/notebooks/v2/6e0860d0-6ddb-4835-85df-4c3dd2916c4d/view?access_token=4d7228b993ae628bba5768a41e703a839e5814118fedcfd6ea71114326f863b4  
